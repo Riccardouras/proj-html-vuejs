f2025a7bfff6e8a6776bc024707ed46372fd57fa
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-05-31 19:02:24 +0200

    end of the structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,47 @@
 <script>
+import { store } from './data/store.js';
 export default {
-    props: ['foo'],
     components: {
         name: "AppFooter",
     },
     data() {
         return {
+            store
         }
     },
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
 }
 
 </script>
 
 <template>
- 
+   <footer>
+    <div>
+        <div><img :src="getImagePath(`../assets/images/${store.Logo}`)" alt=""></div>
+        <div>
+            <div>
+                <h3>Follow Us</h3>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h5>
+                Subcribe to get the latest news, course updates, discounts, events
+            </h5>
+            <input type="email" placeholder="Enter your email here*"> 
+            <br>
+            <button>Subscribe</button>
+        </div>
+    </div>
+   </footer>
 </template>
 
 <style scoped>
